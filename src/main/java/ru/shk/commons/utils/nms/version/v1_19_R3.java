036f23a3d57ce73d4e6d03249c81b1f67f1ed91e
shoker537 <andreybest2002@ya.ru>
2023-04-22 07:31:35 +0300

    New Entity Metadata packet constructor on 1.19.4

--- src/main/java/ru/shk/commons/utils/nms/version/v1_19_R3.java
+++ src/main/java/ru/shk/commons/utils/nms/version/v1_19_R3.java
@@ -1,6 +1,23 @@
 package ru.shk.commons.utils.nms.version;
 
+import lombok.SneakyThrows;
+import net.minecraft.network.protocol.Packet;
+import org.apache.commons.lang.reflect.ConstructorUtils;
+import org.bukkit.entity.Player;
+import ru.shk.commons.utils.nms.FieldMappings;
 import ru.shk.commons.utils.nms.Version;
 
+import java.lang.reflect.Method;
+
 public class v1_19_R3 extends Version {
+    @Override@SneakyThrows
+    protected void entityMetadata(Player p, Object e) {
+        Class<?> packet = Class.forName("net.minecraft.network.protocol.game.PacketPlayOutEntityMetadata");
+        Object dataWatcher = e.getClass().getMethod(FieldMappings.ENTITY_GETDATAWATCHER.getField()).invoke(e);
+        int id = (int) e.getClass().getMethod(FieldMappings.ENTITY_GETID.getField()).invoke(e);
+        Method m = dataWatcher.getClass().getDeclaredMethod("packAll");
+        m.setAccessible(true);
+        Packet<?> pk = (Packet<?>) ConstructorUtils.invokeConstructor(packet, new Object[]{id, m.invoke(dataWatcher)});
+        sendPacket(p, pk);
+    }
 }
