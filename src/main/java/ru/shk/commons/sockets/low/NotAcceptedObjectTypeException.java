8621203798c2a64a58803eef9c575d6978d5af62
shoker537 <andreybest2002@ya.ru>
2023-05-04 18:52:42 +0300

    Removed unused socket-api

--- src/main/java/ru/shk/commons/sockets/low/NotAcceptedObjectTypeException.java
+++
@@ -1,8 +0,0 @@
-package ru.shk.commons.sockets.low;
-
-public class NotAcceptedObjectTypeException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Unrecognized Object type provided in socket message";
-    }
-}
