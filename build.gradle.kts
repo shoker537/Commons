ff1585e6ddf98c3176c50d927dd9e9d16aeb6a54
shoker137 <andreybest2002@ya.ru>
2024-08-31 14:41:07 +0300

    v1.7.2.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "ru.shk"
-version = "1.7.2.2"
+version = "1.7.2.3"
 
 paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.MOJANG_PRODUCTION
 
@@ -90,7 +90,7 @@ dependencies {
   compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.9")
   compileOnly("com.sk89q.worldedit:worldedit-core:7.2.0-SNAPSHOT")
   implementation("commons-io:commons-io:2.11.0")
-  compileOnly("dev.simplix:protocolize-api:2.3.3")
+  compileOnly("dev.simplix:protocolize-api:2.4.1")
   implementation("net.wesjd:anvilgui:1.9.3-SNAPSHOT")
   compileOnly("org.apache.logging.log4j:log4j-core:2.17.1")
   implementation("org.apache.commons:commons-lang3:3.14.0")
