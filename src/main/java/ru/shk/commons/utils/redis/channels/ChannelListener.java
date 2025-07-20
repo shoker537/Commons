8787464ca1650bb1db8ba670d5664ccfea423016
shoker537 <andreybest2002@ya.ru>
2025-07-20 18:38:08 +0300

    Better redis connection

--- src/main/java/ru/shk/commons/utils/redis/channels/ChannelListener.java
+++ src/main/java/ru/shk/commons/utils/redis/channels/ChannelListener.java
@@ -2,68 +2,78 @@ package ru.shk.commons.utils.redis.channels;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
 import lombok.Getter;
 import lombok.experimental.Accessors;
-import redis.clients.jedis.JedisPooled;
+import org.bukkit.Bukkit;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
-import ru.shk.commons.utils.Logger;
 import ru.shk.commons.utils.redis.RedisCredentials;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
 @Accessors(fluent = true)
-public class ChannelListener extends JedisPubSub {
+public class ChannelListener {
     private static final Gson gson = new Gson();
     private final ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
     @Getter private final String channel;
     private final Consumer<JsonObject> onMessage;
-    private JedisPooled jedis;
-    private boolean active = true;
+    private final RedisCredentials redisCredentials;
+    private final AtomicBoolean active = new AtomicBoolean(true);
+    private final Thread thread;
 
     public ChannelListener(RedisCredentials redisCredentials, String channel, Consumer<JsonObject> onMessage) {
         this.channel = channel;
         this.onMessage = onMessage;
-        new Thread(() -> {
-            while (active) {
-                jedis = redisCredentials.newJedis(1,1,1);
-                jedis.subscribe(this, channel);
+        this.redisCredentials = redisCredentials;
+        thread = new Thread(this::startListening);
+        thread.start();
+    }
+
+    public void startListening() {
+        while (active.get()) {
+            try (Jedis jedis = redisCredentials.newJedis()) {
+                System.out.println("Подключение к Redis...");
+                JedisPubSub jedisPubSub = new JedisPubSub() {
+                    @Override
+                    public void onMessage(String channel, String message) {
+                        if (!channel.equalsIgnoreCase(ChannelListener.this.channel)) return;
+                        JsonObject o;
+                        try {
+                            o = gson.fromJson(message, JsonObject.class);
+                        } catch (JsonSyntaxException e) {
+                            return;
+                        }
+                        if (onMessage != null) {
+                            if(!Bukkit.isStopping()) threadPool.submit(() -> onMessage.accept(o));
+                        }
+                    }
+                };
+
+                jedis.subscribe(jedisPubSub, channel);
+            } catch (Exception e) {
+                System.err.println("Ошибка соединения: " + e.getMessage());
+                System.err.println("Повторная попытка подключения через 5 секунд...");
+                try {
+                    Thread.sleep(5000); // ждем перед повторной попыткой
+                } catch (InterruptedException ie) {
+                    return;
+                }
             }
-        }).start();
+        }
     }
 
     public void shutdown(){
+        active.set(false);
+        if (thread!=null) thread.interrupt();
         threadPool.shutdown();
         try {
             threadPool.awaitTermination(10, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        active = false;
-        if (jedis==null) return;
-        try {
-            unsubscribe();
-        } catch (Throwable t){
-
-        }
-        try {
-            jedis.close();
-        } catch (Throwable r){
-
-        }
-    }
-
-    @Override
-    public void onMessage(String channel, String message) {
-        threadPool.submit(() -> {
-            try {
-                JsonObject o = gson.fromJson(message, JsonObject.class);
-                onMessage.accept(o);
-            } catch (Throwable t){
-                Logger.warning("Error processing message from redis:");
-                t.printStackTrace();
-            }
-        });
     }
 }
